6e436e421ebe7bc2f8bfa1ff14a00d054efc698f
Paul Barroso <paulbg1993@gmail.com>
2020-11-23 22:18:48 +0100

    added EsLint

--- src/public/client.js
+++ src/public/client.js
@@ -37,10 +37,9 @@ const App = (state) => {
 
 const mainFunction = (state) => {
 
-    // Call the API if there is no info // Trying with curiosity
-    if (!state.info) {
+    // Call the API if there is no info // Trying with spirit
+    if (state.info == undefined || !state.info) {
         getInfoRovers(state.rovers[2]);
-        console.log('funct working js43') //OK
     }
 
     //console.log(state.rovers[0]) works
@@ -56,8 +55,8 @@ const mainFunction = (state) => {
     // Same day for all the photos [0]
     const date = photos[0].earth_date;
 
-    // Get the required mission data to paint inside this function return
-    const { name, launch_date, landing_date, status } = photos[0].rover
+    // Get the required mission data to paint inside the main function return
+    const { name, launch_date, landing_date, status } = photos[0].rover;
 
     // Print the HTML and add buttons onclick event to fetch API
     return `
@@ -68,10 +67,7 @@ const mainFunction = (state) => {
         <div id="col-rover-0" class="col-lg-4">
 
             <img src="../public/assets/curiosity.jpeg" class="bd-placeholder-img rounded-circle" width="140" height="140"  xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text></img>
-            <h2>${name}</h2>
-            <h2>${launch_date}</h2>
-            <h2>${landing_date}</h2>
-            <h2>${status}</h2>
+            <h2>${name} ${launch_date} ${landing_date} ${status}</h2>
             <p><a id="btn-0" onclick="createTags(${state})" class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
 
         </div>
@@ -100,7 +96,7 @@ const mainFunction = (state) => {
 
 // ------------------------------------------------------  HIGUER ORDER FUNCTIONS
 
-const createTags = (state, ) => {
+const createTags = (state) => {
     let containerIntro = document.getElementById('container-intro');
     containerIntro.style.display = 'none';
     return `` //here comes the rover grid
@@ -115,10 +111,10 @@ const photoHTML = (url) => `<img class="photo" src="${url}"/>`;
 
 const getInfoRovers = (state) => {
     console.log(state);
-    [roverSelection] = state;// Devuelve undefined no se porqué. Con Object.assign tampoco funciona. JSON.parse no funciona. { [roverSelection] } : state no funciona
+    const { roverSelection } = vault; // Devuelve undefined no se porqué. Con Object.assign tampoco funciona. JSON.parse no funciona. { [roverSelection] } : state no funciona
     console.log('API working');
 
     fetch(`rover-photos/${roverSelection}`)
         .then(res => res.json())
-        .then(info => updateVault(vault, { state }))
+        .then(state => updateVault(vault, { state }))
 }
